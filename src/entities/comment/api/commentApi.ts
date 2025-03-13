eb26e9419c5e5f2e28552856a5822dc8c9604633
ksunnwy <ksusazukova@MacBook-Pro-Ksusa.local>
2025-03-13 21:21:35 +0300

    build fix

--- src/entities/comment/api/commentApi.ts
+++ src/entities/comment/api/commentApi.ts
@@ -1,6 +1,6 @@
-import { IComment } from '../types/commentTypes';
+import { Comment } from '../types/commentTypes';
 
-export const fetchCommentsByPostId = async (postId: number, page: number, limit: number): Promise<IComment[]> => {
+export const fetchCommentsByPostId = async (postId: number, page: number, limit: number): Promise<Comment[]> => {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/posts/${postId}/comments?_page=${page}&_limit=${limit}`
   );
@@ -9,6 +9,6 @@ export const fetchCommentsByPostId = async (postId: number, page: number, limit:
     throw new Error('Не удалось загрузить комментарии');
   }
 
-  const data: IComment[] = await response.json();
+  const data: Comment[] = await response.json();
   return data;
 };
