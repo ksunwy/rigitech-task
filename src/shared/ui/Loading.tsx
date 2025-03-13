1378c73676007f3632b99d22828332046ae60ec5
ksunnwy <ksusazukova@MacBook-Pro-Ksusa.local>
2025-03-13 21:25:19 +0300

    icon path changes

--- src/shared/ui/Loading.tsx
+++ src/shared/ui/Loading.tsx
@@ -1,8 +1,9 @@
+import loadingIcon from '../../shared/icons/loading.svg';
 
 const Loading = () => {
   return (
     <div className="flex flex-col gap-3 items-center justify-center h-40">
-        <img src="/src/shared/icons/loading.svg" alt="loading" width={52} height={52} />
+        <img src={loadingIcon} alt="loading" width={52} height={52} />
         <p>Немного подождите...</p>
     </div>
   )
